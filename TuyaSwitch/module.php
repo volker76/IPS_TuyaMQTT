a8abca9b1735f774d3827f5d578e62155353fe7e
Volker Hänsel <volker@haensel-online.de>
2023-10-09 14:02:22 +0200

    first commit

---
+++ TuyaSwitch/module.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__ . '/../libs/TuyaModule.php';
+
+class ShellyPlug extends TuyaModule
+{
+    public static $Variables = [
+        ['Tuya_State', 'State', VARIABLETYPE_BOOLEAN, '~Switch', [], '', true, true] 
+    ];
+
+    public function RequestAction($Ident, $Value)
+    {
+        switch ($Ident) {
+            case 'Tuya_State':
+                $this->SwitchMode($Value);
+                break;
+            }
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        if (!empty($this->ReadPropertyString('MQTTTopic'))) {
+            $Buffer = json_decode($JSONString);
+            $this->SendDebug('JSON', $Buffer, 0);
+
+            //Für MQTT Fix in IPS Version 6.3
+            if (IPS_GetKernelDate() > 1670886000) {
+                $Buffer->Payload = utf8_decode($Buffer->Payload);
+            }
+
+            if (property_exists($Buffer, 'Topic')) {
+                if (fnmatch('*/DP0', $Buffer->Topic)) {
+                    switch ($Buffer->Payload) {
+                        case 'False':
+                            SetValue($this->GetIDForIdent('Tuya_State'), 0);
+                            break;
+                        case 'True':
+                            SetValue($this->GetIDForIdent('Tuya_State'), 1);
+                            break;
+                    }
+                }
+                
+            }
+        }
+    }
+
+    private function SwitchMode(bool $Value)
+    {
+        $Topic = $this->ReadPropertyString('MQTTTopic') . '/DP1/command';
+        if ($Value) {
+            $Payload = 'true';
+        } else {
+            $Payload = 'false';
+        }
+        $this->sendMQTT($Topic, $Payload);
+    }
+}
---
+++ libs/MQTTHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+trait MQTTHelper
+{
+    protected function sendMQTT($Topic, $Payload)
+    {
+        $resultServer = true;
+        //MQTT Server
+        $Server['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
+        $Server['PacketType'] = 3;
+        $Server['QualityOfService'] = 0;
+        $Server['Retain'] = false;
+        $Server['Topic'] = $Topic;
+        $Server['Payload'] = $Payload;
+        $ServerJSON = json_encode($Server, JSON_UNESCAPED_SLASHES);
+        $ServerJSON = json_encode($Server);
+        $this->SendDebug(__FUNCTION__ . 'MQTT Server', $ServerJSON, 0);
+        $resultServer = @$this->SendDataToParent($ServerJSON);
+
+        if ($resultServer === false) {
+            $last_error = error_get_last();
+            echo $last_error['message'];
+        }
+    }
+}
